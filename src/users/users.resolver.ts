4e09d26ecb57679a44cb4de9050e66c7f1bf68ea
geonya <geony8410@gmail.com>
2022-06-14 10:39:36 +0900

    4.3 Create Account Mutation 1

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -9,4 +13,6 @@ export class UsersResolver {
   hi() {
     return true;
   }
+  @Mutation((returns) => CreateAccountOutput)
+  createAccount(@Args('input') createAccountInput: CreateAccountInput) {}
 }
