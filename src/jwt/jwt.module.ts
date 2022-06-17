de095da1e9d49e1991919eeac3bddc6b5588e8b7
geonya <geony8410@gmail.com>
2022-06-17 17:45:25 +0900

    5.3 jwt module 2

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { JwtService } from './jwt.service';
 
 @Module({})
-export class JwtModule {}
+@Global() // 전역적으로 사용 가능
+export class JwtModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: JwtModule,
+      exports: [JwtService],
+      providers: [JwtService],
+    };
+  }
+}
