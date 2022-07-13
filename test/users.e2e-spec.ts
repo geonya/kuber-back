bb1c36326f91711e5a2954b6ea640cd0e7bbfb2a
geonya <geony8410@gmail.com>
2022-07-13 15:23:57 +0900

    9.2 e2e testing create account test & dropdatabase error fix

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import * as request from 'supertest';
+import { DataSource } from 'typeorm';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -9,14 +13,53 @@ describe('UserModule (e2e)', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = module.createNestApplication();
     await app.init();
+  });
+  afterAll(async () => {
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'geony',
+      password: '1234',
+      database: 'kuber-test',
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase();
+    await app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'geony@geony.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"1234",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
   });
 
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
+
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
