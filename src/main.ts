fd1c1ed34ab05df637a47237802e512aa4e08b03
geonya <geony8410@gmail.com>
2022-07-13 13:15:29 +0900

    e2e testing setup

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(PORT, () =>
+    console.log(`http://localhost:${PORT}/graphql server running! 🚀`),
+  );
 }
 bootstrap();
---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('me');
+  it.todo('userProfile');
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+});
